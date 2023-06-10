a48c0a49fa5ea58119a999e771c393bc595bd61e
Duwadi <kduwadi@nrel.gov>
2023-06-09 18:02:38 -0600

    added the ability to run multiple time groups of simulations

--- emerge/cli/custom_metrics.py
+++ emerge/cli/custom_metrics.py
@@ -5,6 +5,7 @@ import datetime
 import yaml
 from pathlib import Path
 import multiprocessing
+from typing import Dict
 
 from emerge.metrics.time_series_metrics import system_metrics
 from emerge.metrics.time_series_metrics import observer
@@ -13,93 +14,75 @@ from emerge.metrics.time_series_metrics import node_voltage_stats
 from emerge.metrics.time_series_metrics import line_loading_stats
 from emerge.metrics.time_series_metrics import xfmr_loading_stats
 
-def _compute_custom_metrics(config, pvsystem_folder_path=None, subfolder=None):
+
+def _get_observers(metrics: Dict):
+    observers = {}
+
+    class_mapper = {
+        'substation_power': system_metrics.TimeseriesTotalPower,
+        'total_pvpower': system_metrics.TimeseriesTotalPVPower,
+        'total_powerloss': system_metrics.TimeseriesTotalLoss,
+        'node_voltage_stats': node_voltage_stats.NodeVoltageStats,
+        'node_voltage_bins': node_voltage_stats.NodeVoltageBins,
+        'line_loading_stats': line_loading_stats.LineLoadingStats,
+        'line_loading_bins': line_loading_stats.LineLoadingBins,
+        'xfmr_loading_stats': xfmr_loading_stats.XfmrLoadingStats,
+        'xfmr_loading_bins': xfmr_loading_stats.XfmrLoadingBins,
+        'overloaded_lines': line_loading_stats.OverloadedLines,
+        'overloaded_transformers': xfmr_loading_stats.OverloadedTransformers,
+        'sardi_voltage': system_metrics.SARDI_voltage,
+        'sardi_line': system_metrics.SARDI_line,
+        'sardi_xfmr': system_metrics.SARDI_transformer,
+        'sardi_aggregated': system_metrics.SARDI_aggregated
+    }
+
+    for key, subdict in metrics.items():
+        if key in class_mapper:
+            if 'args' not in subdict:
+                observers[key] = class_mapper[key]()
+            else:
+                observers[key] = class_mapper[key](**subdict['args'])
+    return observers
+
+def _run_timeseries_sim(
+        master_dss_file_path: str,
+        start_time: str, 
+        end_time: str, 
+        profile_start_time: str,
+        resolution_min: float,
+        export_path: str,
+        pvshape = None,
+        pvsystem_folder_path = None,
+        time_group = None,
+        scenario_folder = None
+):
     date_format = "%Y-%m-%d %H:%M:%S"
     manager = simulation_manager.OpenDSSSimulationManager(
-        config["master"],
-        datetime.datetime.strptime(config["start_time"], date_format),
-        datetime.datetime.strptime(config["profile_start"], date_format),
-        datetime.datetime.strptime(config["end_time"], date_format),
-        config["resolution_min"],
-    )
+                master_dss_file_path,
+                datetime.datetime.strptime(start_time, date_format),
+                datetime.datetime.strptime(profile_start_time, date_format),
+                datetime.datetime.strptime(end_time, date_format),
+                resolution_min,
+            )
     manager.opendss_instance.set_max_iteration(200)
     subject = observer.MetricsSubject() 
 
     if pvsystem_folder_path:
         pvsystem_file_path = pvsystem_folder_path / 'PVSystems.dss'
         manager.opendss_instance.execute_dss_command(f"Redirect {pvsystem_file_path}")
-        manager.opendss_instance.execute_dss_command(f"batchedit pvsystem..* yearly={config['multi_scenario']['pv_profile_shape']}")
+        manager.opendss_instance.execute_dss_command(f"batchedit pvsystem..* yearly={pvshape}")
 
-
-    observers = {}
-    if 'substation_power' in config['metrics']:
-        observers['substation_power'] = system_metrics.TimeseriesTotalPower()
-
-    if 'total_pvpower' in config['metrics']:
-        observers['total_pvpower'] = system_metrics.TimeseriesTotalPVPower()
-    
-    if 'total_powerloss' in config['metrics']:
-        observers['total_powerloss'] = system_metrics.TimeseriesTotalLoss()
-    
-    if 'node_voltage_stats' in config['metrics']:
-        observers['node_voltage_stats'] = node_voltage_stats.NodeVoltageStats()
-
-    if 'node_voltage_bins' in config['metrics']:
-        observers['node_voltage_bins'] = node_voltage_stats.NodeVoltageBins(
-            config['metrics']['node_voltage_bins']['voltage_bins']
-        )
-    if 'line_loading_stats' in config['metrics']:
-        observers['line_loading_stats'] = line_loading_stats.LineLoadingStats()
-
-    if 'line_loading_bins' in config['metrics']:
-        observers['line_loading_bins'] = line_loading_stats.LineLoadingBins(
-            config['metrics']['line_loading_bins']['loading_bins']
-        )
-
-    if 'xfmr_loading_stats' in config['metrics']:
-        observers['xfmr_loading_stats'] = xfmr_loading_stats.XfmrLoadingStats()
-
-    if 'xfmr_loading_bins' in config['metrics']:
-        observers['xfmr_loading_bins'] = xfmr_loading_stats.XfmrLoadingBins(
-            config['metrics']['xfmr_loading_bins']['loading_bins']
-        )
-    if 'overloaded_lines' in config['metrics']:
-        observers['overloaded_lines'] = line_loading_stats.OverloadedLines()
-
-    if 'overloaded_transformers' in config['metrics']:
-        observers['overloaded_transformers'] = xfmr_loading_stats.OverloadedTransformers()
-
-    if 'sardi_voltage' in config['metrics']:
-        observers['sardi_voltage'] = system_metrics.SARDI_voltage(
-            config['metrics']['sardi_voltage']["ov_threshold"], 
-            config['metrics']['sardi_voltage']['uv_threshold']
-        )
-    
-    if 'sardi_line' in config['metrics']:
-        observers['sardi_line'] = system_metrics.SARDI_line(
-            config['metrics']['sardi_line']["thermal_threshold"])
-    if 'sardi_xfmr' in config['metrics']:
-        observers['sardi_xfmr'] = system_metrics.SARDI_transformer(
-             config['metrics']['sardi_xfmr']["thermal_threshold"]
-        )
-    if 'sardi_aggregated' in config['metrics']:
-        observers['sardi_aggregated'] = system_metrics.SARDI_aggregated(
-            loading_limit= config['metrics']['sardi_aggregated']["thermal_threshold"],
-            voltage_limit={
-                "overvoltage_threshold": config['metrics']['sardi_aggregated']["ov_threshold"],
-                "undervoltage_threshold": config['metrics']['sardi_aggregated']["uv_threshold"],
-            },
-        )
-
-
+    observers = _get_observers()
     for _, observer_ in observers.items():
         subject.attach(observer_)
 
     manager.simulate(subject)
 
-    export_base_path = Path(config['export_path']) 
-    if subfolder:
-        export_base_path = export_base_path / subfolder
+    export_base_path = Path(export_path) 
+    if scenario_folder:
+        if time_group:
+            scenario_folder = f"{scenario_folder}_{time_group}"
+        export_base_path = export_base_path / scenario_folder 
     
     if pvsystem_folder_path:
         export_base_path = export_base_path / pvsystem_file_path.parent.name
@@ -110,6 +93,37 @@ def _compute_custom_metrics(config, pvsystem_folder_path=None, subfolder=None):
     manager.export_convergence(export_base_path / 'convergence_report.csv')
     observer.export_csv(list(observers.values()), export_base_path)
 
+def _compute_custom_metrics(config, pvsystem_folder_path=None, scenario_folder=None):
+    
+
+    if config['multi_time']:
+        for time_group, time_dict in config['multi_time'].items():
+            _run_timeseries_sim(
+                config["master"],
+                time_dict['start_time'],
+                time_dict['end_time'],
+                config["profile_start"],
+                config["resolution_min"],
+                config["export_path"],
+                config['multi_scenario']['pv_profile_shape'],
+                pvsystem_folder_path,
+                time_group,
+                scenario_folder
+            )
+    else:
+        _run_timeseries_sim(
+                config["master"],
+                config['start_time'],
+                config['end_time'],
+                config["profile_start"],
+                config["resolution_min"],
+                config["export_path"],
+                config['multi_scenario']['pv_profile_shape'],
+                pvsystem_folder_path,
+                None,
+                scenario_folder
+            )
+            
 
 @click.command()
 @click.option(
